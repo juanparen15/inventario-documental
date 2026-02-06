013513929f65df331e8a8c0c4c043b0d2be62532
juanparen15 <jprendon9@gmail.com>
2026-02-06 07:33:20 -0500

    remove sortable

--- app/Filament/Resources/InventoryRecordResource.php
+++ app/Filament/Resources/InventoryRecordResource.php
@@ -319,7 +319,6 @@ class InventoryRecordResource extends Resource
                             ->downloadable()
                             ->openable()
                             ->reorderable()
-                            ->sortable()
                             ->helperText('Solo archivos PDF. Maximo 20MB por archivo.')
                             ->columnSpanFull(),
                     ]),
